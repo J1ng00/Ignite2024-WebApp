a4c4c93ac34e9a1c8237661d6169d63480635fd4
J1ng00 <wongjl@hotmail.com>
2024-09-09 15:56:43 +0800

    Install Postgres

--- mvc/main.go
+++ mvc/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/J1ng00/Ignite2024-WebApp/mvc/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDatabase()
+}
 
 func main() {
 	fmt.Println("Hello")
